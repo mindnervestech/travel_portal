338b42f9e0f92b71d2b7291229129a35ace66c23
yogesh <yogesh_337@yahoo.com>
2014-09-29 21:32:12 +0530

    general_info

--- app/com/travelportal/controllers/TravelPortalUtilsController.java
+++ app/com/travelportal/controllers/TravelPortalUtilsController.java
@@ -1,15 +1,23 @@
 package com.travelportal.controllers;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+
+import play.data.DynamicForm;
 import play.db.jpa.Transactional;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.base.Splitter;
 import com.travelportal.domain.City;
 import com.travelportal.domain.Country;
 import com.travelportal.domain.HotelChain;
+import com.travelportal.vm.HotelGeneralInfoVM;
 
 public class TravelPortalUtilsController extends Controller {
 	
@@ -19,6 +27,7 @@ public class TravelPortalUtilsController extends Controller {
 		return ok(Json.toJson(countries));
 	}
 	
+	@Transactional
 	public static Result getCities(int countryCode) {
 		List<City> cities = City.getCities(countryCode);
 		return ok(Json.toJson(cities));
@@ -28,7 +37,40 @@ public class TravelPortalUtilsController extends Controller {
 		List<HotelChain> cities = HotelChain.getChainHotels();
 		return ok(Json.toJson(cities));
 	}
+	   
+   
+   @Transactional
+   public static Result getCityname(int countryCode){
+   	List<City> listcity = City.getCities(countryCode); 
+   	List<Map> list = new ArrayList<>();
+   	
+   		for(City city : listcity){
+   			Map<String,Object> map = new HashMap<String, Object>();
+   			map.put("cityname", city);
+   			list.add(map);
+   		}
+   		return ok(Json.toJson(list));
+      
+   }
 	
+   
+   @Transactional
+	public static Result saveGeneralInfo() {
+	   
+	   
+	 /*  System.out.println("//////////8888////////");
+	   System.out.println("hiiiiiiiiiiiiiiii");
+	   System.out.println("//////////////////");
+		JsonNode json = request().body().asJson();
+		DynamicForm form = DynamicForm.form().bindFromRequest();
+		System.out.println(form);
+		Json.fromJson(json, HotelGeneralInfoVM.class);
+		HotelGeneralInfoVM hotelGeneralInfoVM = Json.fromJson(json, HotelGeneralInfoVM.class);
+				
+		
+		newspaperdetails.save();*/
+		return ok();
+	}
 	
 	
 }
--- public/lib/controllers.js
+++ public/lib/controllers.js
@@ -1,8 +1,40 @@
-angular.module('travel_portal_hotel_profile').
-	controller("generalInfoProfileController",['$scope', '$http',function($scope, $http){
-	   $scope.countries = [];
+angular.module('travel_portal_hotel_profile').controller("generalInfoProfileController",['$scope', '$http',function($scope, $http,getCityNameservice){
+	   
+	
+	
+	$scope.countries = [];
 	   $http.get("/countries").success(function(response) {
 		   console.log("getting countries...");
 		   $scope.countries = response;
-	   });                             
+	   });                
+	   
+	   
+	   $scope.savegeneralinfo = function() {
+			console.log($scope.generalInfo);
+			$http.post('/saveGeneralInfo', $scope.generalInfo).success(function(data){
+				console.log('success');
+				/*$scope.searchBasicRate(currentPage);
+				$('#myModal').modal('hide');*/
+			}).error(function(data, status, headers, config) {
+				console.log('ERROR');
+			});
+		};
+	   
+	    $scope.onStateselect = function() {
+			console.log($scope.generalInfo.countryCode.countryCode);
+			
+			$http.get('getCityname/'+$scope.generalInfo.countryCode.countryCode)
+			.success(function(data){
+				if(data) {
+					$scope.cityname = data;
+					console.log($scope.cityname);
+					
+				} else {
+					$scope.cityname = [];
+				}
+			});
+			
+			}
+	   
+	   
     }]);
