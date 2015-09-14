86e95e1222086db2644c62f630eb4fd8997a05e9
ankit <asbharadiya@gmail.com>
2015-09-14 12:23:26 +0530

    solv error

--- app/controllers/travelbusiness/BookingConfirmRejectController.java
+++ app/controllers/travelbusiness/BookingConfirmRejectController.java
@@ -1,10 +1,30 @@
 package controllers.travelbusiness;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Properties;
 
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+import play.Play;
 import play.db.jpa.Transactional;
 import play.libs.Json;
 import play.mvc.Controller;
@@ -27,6 +47,74 @@ public class BookingConfirmRejectController extends Controller {
 		hBookingDetails.setConfirmationId(confirmationId);
 		hBookingDetails.setConfirmProcessUser(nameConfirm);
 		hBookingDetails.merge();
+		
+		String voucherPdfFile = rootDir + File.separator + "BookingVoucherDocuments"+File.separator+ bookingId + File.separator+"HotelVoucher.pdf";
+	 	File f = new File(voucherPdfFile);
+		
+		AgentRegistration aRegistration = AgentRegistration.findByIdOnCode(hBookingDetails.getAgentId().toString());
+		if(status.equals("Confirm")){
+			
+			final String username=Play.application().configuration().getString("username");
+	        final String password=Play.application().configuration().getString("password");
+	           
+	        Properties props = new Properties();
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.host", "smtp.checkinrooms.com");
+			props.put("mail.smtp.port", "587");
+			props.put("mail.smtp.starttls.enable", "true");
+			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(username, password);
+				}
+			});
+			try
+			{
+				MimeBodyPart attachPart = new MimeBodyPart();
+				Message message = new MimeMessage(session);
+				message.setFrom(new InternetAddress(username));
+				message.setRecipients(Message.RecipientType.TO,
+						InternetAddress.parse(aRegistration.getEmailAddr()));
+				message.setSubject("Confirmation Of Booking");
+				Multipart multipart = new MimeMultipart();
+				BodyPart messageBodyPart = new MimeBodyPart();
+				messageBodyPart = new MimeBodyPart();
+				
+				VelocityEngine ve = new VelocityEngine();
+				ve.setProperty( RuntimeConstants.RUNTIME_LOG_LOGSYSTEM_CLASS,"org.apache.velocity.runtime.log.Log4JLogChute" );
+				ve.setProperty("runtime.log.logsystem.log4j.logger","clientService");
+				ve.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath"); 
+				ve.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+				ve.init();
+			
+				
+		        Template t = ve.getTemplate("/public/emailTemplet/BookingConfirmationMailToAgent.vm"); 
+		        VelocityContext context = new VelocityContext();
+		        context.put("uuId", hBookingDetails.getUuId());
+		        context.put("agentName",hBookingDetails.getAgentNm());
+		        
+		        
+		        StringWriter writer = new StringWriter();
+		        t.merge( context, writer );
+		        String content = writer.toString(); 
+				
+				messageBodyPart.setContent(content, "text/html");
+				multipart.addBodyPart(messageBodyPart);
+				  try {
+						attachPart.attachFile(f);
+			  	      } catch (IOException e) {
+			  	       	// TODO Auto-generated catch block
+			  	       		e.printStackTrace();
+			  	    }
+				 multipart.addBodyPart(attachPart);
+				message.setContent(multipart);
+				Transport.send(message);
+				System.out.println("Sent test message successfully....");
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			} 
+		}
 		
 		 return ok();
 	 }
