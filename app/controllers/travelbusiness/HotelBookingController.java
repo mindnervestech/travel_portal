beaa189fd521799a504366a52a273930cf9d9c39
“abc” <harshad.r.gojare@gmail.com>
2016-08-08 17:36:08 +0530

    change mail url

--- app/controllers/travelbusiness/HotelBookingController.java
+++ app/controllers/travelbusiness/HotelBookingController.java
@@ -2518,12 +2518,12 @@ public class HotelBookingController extends Controller {
 	        Properties props = new Properties();
 			props.put("mail.smtp.auth", "true");
 			props.put("mail.smtp.starttls.enable", "true");
-			props.put("mail.smtp.host", "smtp.checkinrooms.com");
+			props.put("mail.smtp.host", "smtp.gmail.com");
 			props.put("mail.smtp.port", "587");
 			
 			Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 				protected PasswordAuthentication getPasswordAuthentication() {
-					return new PasswordAuthentication(username, password);
+					return new PasswordAuthentication("yogeshpatil424@gmail.com", "337@yogesh");
 				}
 			});
 			try
@@ -2704,7 +2704,7 @@ public class HotelBookingController extends Controller {
 			{
 				//MimeBodyPart attachPart = new MimeBodyPart();
 				Message message = new MimeMessage(session);
-				message.setFrom(new InternetAddress(username,password));
+				message.setFrom(new InternetAddress("yogeshpatil424@gmail.com","337@yogesh"));
 				message.setRecipients(Message.RecipientType.TO,
 						InternetAddress.parse(aRegistration.getEmailAddr()));
 				message.setSubject("Confirmation Of Booking Agent");
